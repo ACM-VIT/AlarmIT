fb06fabb9eba59a0ef3964bd70f8d158f4264ef2
rudrankbasant <rudrank123@gmail.com>
2022-10-05 13:46:39 +0530

    initial commit

---
+++ app/src/main/java/com/example/alarmit/ui/EditAlarmFragment.kt
@@ -0,0 +1,23 @@
+package com.example.alarmit.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.alarmit.R
+
+
+class EditAlarmFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_alarm, container, false)
+    }
+
+
+}
